5baf602b2e00045371af2f1b8d84c1b875017ed6
theordinaryguy23 <100566286+theordinaryguy23@users.noreply.github.com>
2026-06-11 12:58:22 +0700

    Bump version to 1.8.4

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,8 +4,8 @@ object AndroidVersions {
     const val compileSdk = 36
     const val minSdk = 23
     const val targetSdk = 36
-    const val versionCode = 114
-    const val versionName = "1.8.3"
+    const val versionCode = 115
+    const val versionName = "1.8.4"
     const val ndk = "23.1.7779620"
     const val kotlin = "2.3.10"
 }
