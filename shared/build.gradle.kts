2b7c6ba5522f8859c876ae996e9bb137c7112c35
theordinaryguy23 <100566286+theordinaryguy23@users.noreply.github.com>
2026-09-24 17:57:20 +0700

    test shared core with coroutines test support

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -10,5 +10,7 @@ kotlin {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 }
