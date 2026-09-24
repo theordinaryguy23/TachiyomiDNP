db1ea02f36ac2338ae3bcd16e05ef438d9240376
theordinaryguy23 <100566286+theordinaryguy23@users.noreply.github.com>
2026-09-24 17:56:37 +0700

    feat: add shared core module foundation

---
+++ shared/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+}
